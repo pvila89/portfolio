bd047a70a769b27ea7ed9714e105a52481cc11fd
Pau Vila <pvila89@gmail.com>
2023-05-18 17:04:30 +0200

    Update to my own portfolio, setting heading, hero sections

--- js/main.js
+++ js/main.js
@@ -4,3 +4,15 @@ collapsibles.forEach((item) =>
     this.classList.toggle("collapsible--expanded");
   })
 );
+
+document.addEventListener('DOMContentLoaded', function() {
+  var smoothScrollLinks = document.getElementsByClassName('smooth-scroll');
+  for (var i = 0; i < smoothScrollLinks.length; i++) {
+    smoothScrollLinks[i].addEventListener('click', function(event) {
+      event.preventDefault();
+      var target = document.querySelector(this.getAttribute('href'));
+      target.style.opacity = "1";
+      target.scrollIntoView({ behavior: 'smooth' });
+    });
+  }
+});
